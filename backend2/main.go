c1e6d72864f017c677da5ba4e329ecbe62875f83
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-01-30 12:56:20 +0000

    [ja] apply patch]

--- backend2/main.go
+++ backend2/main.go
@@ -203,3 +203,44 @@ func UploadDataParser(w http.ResponseWriter, r *http.Request, p httprouter.Param
 
 	http.Redirect(w, r, presignedURL.String(), http.StatusTemporaryRedirect)
 }
+
+func GetModel(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := r.FormValue("id")
+	if id == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	reqParams := make(url.Values)
+	expiry := time.Second * 120
+	presignedURL, err := minioClient.PresignedGetObject(id, "model", expiry, reqParams)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, presignedURL.String(), http.StatusTemporaryRedirect)
+}
+
+func GetData(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	model := r.FormValue("model")
+	if model == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	id := r.FormValue("id")
+	if id == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	reqParams := make(url.Values)
+	expiry := time.Second * 120
+	presignedURL, err := minioClient.PresignedGetObject(model, "data:"+id, expiry, reqParams)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, presignedURL.String(), http.StatusTemporaryRedirect)
