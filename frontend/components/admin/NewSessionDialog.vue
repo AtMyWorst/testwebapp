f0afaa8e9054e379c1da0753957f65812044fe4d
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-05-09 13:23:11 +0000

    nfsd: Fix ACL null pointer deref

---
+++ frontend/components/admin/NewSessionDialog.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div
+    class="fader fixed inset-0 p-6 flex items-center justify-center"
+    v-if="show"
+  >
+    <div class="bg-white rounded-card shadow-md m-6">
+      <div
+        class="bg-primary text-white rounded-card flex items-center justify-start"
+      >
+        <button class="m-5" @click="$emit('update:show', false)">
+          <fa-icon size="2x" :icon="['far', 'times']" />
+        </button>
+        <h1 class="text-2xl font-bold flex-1">New Session</h1>
+        <button class="m-5 p-1" @click="submit">
+          Create
+        </button>
+      </div>
+      <div class="p-2">
+        <div class="m-4">
+          <input
+            class="text-2xl font-bold"
+            v-model="sessionTitle"
+            placeholder="Enter session name"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionModel = $event"
+            placeholder="Drop model here"
+            label="Model"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionData = $event"
+            placeholder="Drop data here"
+            label="Data"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionLabels = $event"
+            placeholder="Drop labels here"
+            label="Labels"
