c778bc90f53ac4d0a21af9eaf67666b9606a2e01
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-06-25 13:53:12 +0000

    Version 1.1r34

--- frontend/components/common/Subcard.vue
+++ frontend/components/common/Subcard.vue
@@ -2,4 +2,4 @@
   <div class="flex flex-col mx-6">
     <div class="flex items-center">
       <div class="flex-grow">
-        <h1 v-if="titl
+        <h1 v-if="title" class="font-bold text-2xl">{{ title 
