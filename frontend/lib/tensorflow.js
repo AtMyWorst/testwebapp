c090f01999c38043087f1639c089a285897ec1a0
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-09-21 19:36:02 +0000

    udf: Fortify loading of sparing table

--- frontend/lib/tensorflow.js
+++ frontend/lib/tensorflow.js
@@ -27,4 +27,6 @@ class DistTensorflow {
   tf.loadLayersModel(`${this.base2}/model?id=${this.modelId}`).then(function (model) {
     this.model = model;
 
-    // Compile the model with default optimizer an
+    // Compile the model with default optimizer and loss
+    this.model.compile({
+    
