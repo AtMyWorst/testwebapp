f65c4d550c610d777456de3ca08d5d5f6d4e33d0
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-11-29 12:43:44 +0000

    i18n fixes.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,4 +9,4 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
+      { hid: 'description', name: 'des
