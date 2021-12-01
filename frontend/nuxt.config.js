5f234ea7f92dc7079aabb31991d4669d9d382abc
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-12-01 06:50:50 +0000

    USB: ftdi-sio: add support for more Physik Instrumente devices

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,4 +9,5 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.e
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+   
