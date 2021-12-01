dd66374d7292dce7e3d43dc8a5fe15b61781dab0
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-12-01 18:10:41 +0000

    Add Ansible Galaxy shield

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -10,4 +10,6 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-   
+    ],
+    link: [
+      { r
