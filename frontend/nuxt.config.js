d0cafeb22b27acaa61caf320e258a3cc6c2becf8
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2021-12-02 13:41:25 +0000

    fix after rebase

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -12,4 +12,5 @@ module.exports = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-ico
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 
