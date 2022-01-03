7682004c836e5d4f00bfd1a4219f99bc4de0df9d
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-01-03 22:57:14 +0000

    link correction

---
+++ frontend/pages/_id.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <Header :title="title" subtitle="by MakerForce">
+      <template v-slot:buttons-right>
+        <button v-if="running" class="my-2 ml-4" @click="toggleRunning">
+          <fa-icon size="2x" :icon="['far', 'pause']" />
+        </button>
+        <button v-if="completed" class="my-2 ml-4" @click="toggleRunning">
+        </button>
+        <button v-else class="my-2 ml-4" @click="toggleRunning">
+          <fa-icon size="2x" :icon="['far', 'play']" />
+        </button>
+      </template>
+      <template v-slot:content>
+        <div class="mx-3 my-6">
+          <h2 class="font-medium">Loss</h2>
+          <trend
+            :data="major"
