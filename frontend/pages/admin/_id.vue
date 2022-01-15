b3342906172d681a4962529492f023aafc35ce2d
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-01-15 14:57:58 +0000

    "Add include common.php"

--- frontend/pages/admin/_id.vue
+++ frontend/pages/admin/_id.vue
@@ -2,4 +2,5 @@
   <div>
     <Header :title="title" subtitle="by MakerForce">
       <template v-slot:buttons-left>
-        <router-link class="my-2 mr-4" :to="{ name: '
+        <router-link class="my-2 mr-4" :to="{ name: 'admin' }">
+          <fa-icon size="2x" :
