e81eb20827b5167e5930e5eabd90e312a18c1c31
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-01-16 15:58:23 +0000

    chore(package): update ava to version 0.13.0

--- frontend/pages/admin/_id.vue
+++ frontend/pages/admin/_id.vue
@@ -4,3 +4,5 @@
       <template v-slot:buttons-left>
         <router-link class="my-2 mr-4" :to="{ name: 'admin' }">
           <fa-icon size="2x" :icon="['far', 'arrow-left']" />
+        </router-link>
+      </template>
