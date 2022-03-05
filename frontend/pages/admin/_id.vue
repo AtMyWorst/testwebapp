4dde80d8435a8b710c3310ab96147373da9cdc37
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-03-05 08:58:05 +0000

    Update sectionI.html

--- frontend/pages/admin/_id.vue
+++ frontend/pages/admin/_id.vue
@@ -62,4 +62,4 @@
 import Header from '~/components/common/Header.vue'
 import Cards from '~/components/common/Cards.vue'
 import Card from '~/components/common/Card.vue'
-impo
+import CenteredText from '~/components/commo
