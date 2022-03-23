213d22e0db227232c9b4ae9e1efcc3d72d7d286b
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-03-23 11:13:26 +0000

    simplified

--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -2,3 +2,38 @@
 <template>
   <div>
     <Header title="Sessions">
+      <template v-slot:buttons-right>
+        <button class="my-2 ml-4" @click="showNewSessionDialog = true">
+          <fa-icon size="2x" :icon="['far', 'plus']" />
+        </button>
+      </template>
+    </Header>
+    <Cards>
+      <Card
+        v-for="model in models"
+        :title="model.title"
+        :key="model.title"
+        arrow
+        :to="{ name: 'admin-id', params: { id: model.id } }"
+      >
+        <div class="flex">
+          <Subcard subtitle="Elapsed">
+            <CenteredText class="text-4xl">
+              {{ model.elapsed }}
+            </CenteredText>
+          </Subcard>
+          <Subcard subtitle="Loss">
+            <CenteredText class="text-4xl">
+              {{ model.loss }}
+            </CenteredText>
+          </Subcard>
+        </div>
+      </Card>
+    </Cards>
+    <NewSessionDialog :show.sync="showNewSessionDialog" @needsRefresh="fetch" />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/common/Header.vue'
+import Cards from '~/components/common/Cards.vue'
