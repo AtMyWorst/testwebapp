d5b89ee5254c04fc05fbb2c6e4021b4d89b438a2
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-03-29 10:42:22 +0000

    Update init.pp

--- frontend/plugins/fontawesome.js
+++ frontend/plugins/fontawesome.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fal } from '@fortawes
+import { fal } from '@fortawesome/pro-light-svg-icons'
+import { far }
