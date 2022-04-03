1c84a80eba4a4f454edd8d9e640147e0d91afeca
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-04-03 05:50:16 +0000

    main update

--- frontend/plugins/fontawesome.js
+++ frontend/plugins/fontawesome.js
@@ -2,4 +2,6 @@ import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fal } from '@fortawesome/pro-light-svg-icons'
-import { far }
+import { far } from '@fortawesome/pro-regular-svg-icons'
+
+// T
