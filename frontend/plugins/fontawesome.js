80c48df158800ebbe5e81452674c6459ac848617
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-04-05 10:43:08 +0000

    Remove superfluous comment

--- frontend/plugins/fontawesome.js
+++ frontend/plugins/fontawesome.js
@@ -7,4 +7,5 @@ import { far } from '@fortawesome/pro-regular-svg-icons'
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
-// You can add your icons directly in this plugin. See 
+// You can add your icons directly in this plugin. See other examples for how you
+// 
