a131c30cdeb409a9d574f380769749fa44541a06
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-04-10 16:34:41 +0000

    Remove unneeded imports

--- frontend/plugins/fontawesome.js
+++ frontend/plugins/fontawesome.js
@@ -10,4 +10,6 @@ config.autoAddCss = false
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
 library.add(fal)
-lib
+library.add(far)
+
+// Register the compon
