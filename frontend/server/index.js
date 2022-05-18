8447729bb7636125b9af8e68fc1b721aeab61ddb
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-05-18 16:58:59 +0000

    more hacks

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -28,4 +28,5 @@ async function start() {
   app.use(ctx => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
-    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtSe
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx
