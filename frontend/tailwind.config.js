47be692bec74a6a37b3a4a844b6e015dac103802
AtMyWorst <123346681+AtMyWorst@users.noreply.github.com>
2022-06-05 17:05:53 +0000

    e1000: add dummy allocator to fix race condition between mtu change and netpoll

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,3 +4,16 @@ module.exports = {
   theme: {
     extend: {
 		fontFamily: {
+			sans: ["Inter var", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+		},
+		colors: {
+			primary: '#FF6900',
+			'primary-dark': '#CC5500',
+		},
+		borderRadius: {
+			'card': '1.5rem',
+		},
+	}
+  },
+  variants: {}
+}
